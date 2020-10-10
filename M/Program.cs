57dca76d2cd9e0e40cca35737abca2d3e0bfb281
Phuoc Quach ngoc <phuocquach.nt@gmail.com>
2020-10-10 13:51:28 +0700

    add more solution

---
+++ M/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MaxDoubleSliceSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().solution(new int[] {5, 17, 0, 3}));
+        }
+
+        class Solution
+        {
+            public int solution(int[] A)
+            {
+                var maxFromStart = new int[A.Length];
+                maxFromStart[0] = 0;
+                maxFromStart[1] = 0;
+                maxFromStart[2] = Max(A[1],0);
+
+                for(int i=3;i<A.Length-1;i++)
+                {
+                    maxFromStart[i] = Max(maxFromStart[i - 1] + A[i-1], A[i-1]);
+                    maxFromStart[i] = Max(maxFromStart[i], 0);
+                    //Console.WriteLine(maxFromStart[i]);
+                }
+
+                var maxFromEnd = new int[A.Length];
+                maxFromEnd[A.Length - 1] = 0;
+                maxFromEnd[A.Length - 2] = 0;
+                maxFromEnd[A.Length - 3] = Max(A[^2],0);
+
+                for (int i = A.Length-4;i>=0; i--)
+                {
+                    maxFromEnd[i] = Max(maxFromEnd[i + 1] + A[i + 1], A[i + 1]);
+                    maxFromEnd[i] = Max(maxFromEnd[i], 0);
+                    //Console.WriteLine(maxFromEnd[i]);
+                }
+
+                var max = 0;
+                for(int i = 1;i<A.Length-1;i++)
+                {
+                    max = Max(max, maxFromEnd[i] + maxFromStart[i]);
+                }
+
+                return max;
+            }
+
+            private int Max(int v1, int v2) => v1 > v2 ? v1 : v2;
+        }
+    }
+}
---
+++ MaxDoubleSlice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MaxDoubleSlice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            Console.WriteLine(s.solution(new int[]
+            {
+                3,2,6,-1,4,5,-1,2
+            }));
+        }
+
+        class Solution
+        {
+            public int solution(int[] A)
+            {
+                var max = new int[A.Length];
+                max[2] = A[0] + A[1] + A[2];
+
+                for(int i = 3; i< A.Length;i++)
+                {
+                    max[i] = Max(max[i - 1] + A[i], A[i - 1] + A[i] + A[i - 2]);
+                }
+                var max_ending = max[2];
+                for(int i = 2; i<max.Length;i++)
+                {
+                    max_ending = Max(max_ending, max[i]);
+                }
+                return max_ending;
+            }
+
+            private int Max(int x, int y)
+            {
+                return x > y ? x : y;
+            }
+        }
+    }
+}
